65aa3818b42da531fa0ea6c9fc4f1c0f306dd8d7
Tour India <pritviraj04@gmail.com>
2026-08-22 15:37:14 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,8 @@
-const CONFIG = {
+window.TOUR_INDIA_CONFIG = {
   API_BASE_URL:
-    "https://script.google.com/macros/s/AKfycbwMdd3WRGvPeDdTh_7wXLOlj3vezKVLTFaDBlxcgvqFqILiyhunc3SWHbGc2tF8HO5tNA/exec"
+    "https://script.google.com/macros/s/AKfycbwMdd3WRGvPeDdTh_7wXLOlj3vezKVLTFaDBlxcgvqFqILiyhunc3SWHbGc2tF8HO5tNA/exec",
+
+  REQUEST_TIMEOUT_MS: 15000,
+
+  SESSION_STORAGE_KEY: "tourIndiaSession"
 };
